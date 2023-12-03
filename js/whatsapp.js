7e08e6d77610b9fc3bad9af480088a218a2698e5
sukehighland <124778854+sukehighland@users.noreply.github.com>
2023-12-04 00:24:48 +0800

    Update whatsapp.js

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
-  $("#add_message").change(function() {
-    if (this.checked) {
-      $("#message_field").removeClass("d-none");
-    } else {
-      $("#message_field").addClass("d-none");
+  // Update hidden field on any change in input and select elements
+  $("#name, #nomorhp, #datetime, #select1, #message").on("change input", function() {
+    var text = "Halo, saya ingin memesan meja untuk " + $("#select1").val() + " orang pada tanggal " + $("#datetime").val() + ". Nama saya " + $("#name").val() + " dan nomor telepon saya " + $("#nomorhp").val();
+    if ($("#message").val()) {
+      text += ". Pesan tambahan: " + $("#message").val();
     }
+    $("#pre-filled-text").val("text=" + encodeURIComponent(text));
   });
 });
